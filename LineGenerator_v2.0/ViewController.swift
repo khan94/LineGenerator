fb01c72fffaf626a6716ed35e32fd327ffec5d33
Khan <abylaikhan94@gmail.ru>
2017-03-23 17:06:43 -0700

    Initial Commit

---
+++ LineGenerator_v2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LineGenerator_v2.0
+//
+//  Created by Khan Julmagambetov on 2017-03-23.
+//  Copyright © 2017 AJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
